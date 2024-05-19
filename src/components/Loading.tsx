d222fccc56c6cb3631001a2d8dbfba60634563df
Letícia Garcez <lpgarcez@protonmail.com>
2024-05-18 23:12:01 -0300

    arquivo de carregamento criado

---
+++ src/components/Loading.tsx
@@ -0,0 +1,7 @@
+export default function () {
+	return (
+		<div className="flex items-center justify-center h-screen">
+			<div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-blue-700"></div>
+		</div>
+	);
+}
